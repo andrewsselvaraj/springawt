791d62f0b553390a52742eca39a821fb6b43b147
DELL <DELL@DESKTOP-LF9C4GT>
2023-03-03 18:03:44 +0530

    management.endpoints.web.exposure.include

---
+++ spring-boot-jwt/src/main/java/com/auth/aspect/LoginAspect.java
@@ -0,0 +1,37 @@
+package com.auth.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect  
+@Component
+
+public class LoginAspect {
+	//@Pointcut(value= "execution(* com.efreelearn.login.mvccontroller.LoginController.*(..))")  
+	
+	@Pointcut(value= "execution(*com.auth.controller.JwtAuthenticationController.*(..))")  
+	private void displayLogMessage()   
+	{   
+		System.out.println("ANDREW LOG");
+	}
+	
+	//@Around(value= "displayLogMessage()")  
+	public void aroundAdvice(ProceedingJoinPoint jp) throws Throwable   
+	{  
+	System.out.println("BEFORE METHOD " + jp.getSignature().getName() + " method");  
+	try   
+	{  
+	jp.proceed();  
+	}   
+	finally   
+	{  
+	  
+	}  
+	System.out.println("AFTER METHOD " + jp.getSignature().getName() + " method");  
+	}  
+}  
+	
+
