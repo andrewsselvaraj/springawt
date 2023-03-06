0d41e12c8b314d1d9a28b5fd47ef1521a96ef562
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-07 00:24:55 +0530

    aws hellow ordl

---
+++ spring-boot-jwt/src/test/java/com/jwt/junit/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.jwt.junit;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+public class HomeControllerTest {
+
+    @Test
+    public void testAdd() {
+        
+        assertEquals(1, 1);
+    }
+}
