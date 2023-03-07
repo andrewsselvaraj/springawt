3f17006a42d0eef6baea5159d7b04ed4e29d90e2
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-07 09:13:01 +0530

    Junit

--- spring-boot-jwt/src/test/java/com/jwt/junit/HomeControllerTestMock.java
+++ spring-boot-jwt/src/test/java/com/jwt/junit/HomeControllerTestMock.java
@@ -1,31 +1,32 @@
 package com.jwt.junit;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-@SpringBootTest
+
+import com.auth.SpringBootHelloWorldApplication;
+
 @RunWith(SpringRunner.class)
+@SpringBootConfiguration
+@SpringBootTest(classes = SpringBootHelloWorldApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class HomeControllerTestMock extends SpringBootHelloWorldTests{
 
     @Autowired
     private TestRestTemplate template;
     
+    
+    
     @Test
-	public void testEmployee() throws Exception {
+	public void testController() throws Exception {
     	 ResponseEntity<String> response = template.getForEntity("/hello", String.class);
-         assertThat(response.getBody()).isEqualTo("hello");
+         assertThat(response.getBody()).isEqualTo("Hello World");
 
 	}
 }
